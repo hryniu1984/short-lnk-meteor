c6f05b4b1a7c646d9dc2b179c12d9e9f35ca5398
Piotr Hryniewicz <piotr.hryniewicz84@gmail.com>
2017-07-27 00:48:48 +0200

    Initial commit

---
+++ client/main.js
@@ -0,0 +1,21 @@
+import {Meteor} from 'meteor/meteor';
+import ReactDOM from 'react-dom';
+import { Tracker } from 'meteor/tracker';
+import { Session } from 'meteor/session';
+
+
+import { routes, onAuthChange } from '../imports/routes/routes';
+import '../imports/startup/simple-schema-configuration.js';
+
+
+Tracker.autorun(() => {
+  const isAuthenticated = !!Meteor.userId();
+  onAuthChange(isAuthenticated);
+});
+
+
+
+Meteor.startup(()=>{
+  Session.set('showVisible', false);
+  ReactDOM.render(routes, document.getElementById('app'));
+});
